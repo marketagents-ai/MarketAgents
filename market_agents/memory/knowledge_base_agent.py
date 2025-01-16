71da0a644b63821c9073e7df9eae33146ae8c697
interstellarninja <ninjainterstellar@gmail.com>
2025-01-16 15:39:11 -0500

    channging class to pydantic basemodel

--- market_agents/memory/knowledge_base_agent.py
+++ market_agents/memory/knowledge_base_agent.py
@@ -1,23 +1,22 @@
 from typing import Dict, List, Optional, Any
 from uuid import UUID
+from pydantic import BaseModel, Field
 
 from market_agents.memory.knowledge_base import MarketKnowledgeBase
 from market_agents.memory.vector_search import RetrievedMemory
 from market_agents.memory.vector_search import MemoryRetriever
 
+class KnowledgeBaseAgent(BaseModel):
+    market_kb: MarketKnowledgeBase
+    retriever: MemoryRetriever
+    knowledge_bases: Dict[str, MarketKnowledgeBase] = Field(default_factory=dict)
+    retrieved_knowledge: List[RetrievedMemory] = Field(default_factory=list)
 
-class KnowledgeBaseAgent:
-    def __init__(self, market_knowledge_base: MarketKnowledgeBase, retriever: MemoryRetriever):
-        self.knowledge_bases: Dict[str, MarketKnowledgeBase] = {}
-        self.retrieved_knowledge: List[RetrievedMemory] = []
+    class Config:
+        arbitrary_types_allowed = True
 
-        self.marketKnowledgeBase = market_knowledge_base
-        self.retriever = retriever
+    def store(self, text: str, metadata: Optional[Dict[str, Any]] = None) -> UUID:
+        return self.market_kb.ingest_knowledge(text, metadata)
 
-
-    def store(self, text: str, metadata: Optional[Dict[str, Any]] = None)-> UUID:
-        return self.marketKnowledgeBase.ingest_knowledge(text, metadata)
-
-
-    def retrieve(self, query: str, table_prefix:str)-> List[RetrievedMemory]:
-        return self.retriever.search_knowledge_base(table_prefix, query, self.marketKnowledgeBase.config.top_k)
+    def retrieve(self, query: str, table_prefix: str) -> List[RetrievedMemory]:
+        return self.retriever.search_knowledge_base(table_prefix, query, self.market_kb.config.top_k)
